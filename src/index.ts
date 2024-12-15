d4e7591d91f834c04f2f7c0545fe44655ea7c95a
zBlade <dnguy3n2010@gmail.com>
2024-12-15 14:31:17 -0500

    feat: add xterm addon command handling

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,28 @@ export * from "./computer/computer";
 export * from "./computer/user/user";
 export * from "./computer/user/privileges";
 
-export * from "filesystem/directory";
-export * from "filesystem/file";
-export * from "filesystem/filesystem";
-export * from "filesystem/path/path";
+export * from "./filesystem/directory";
+export * from "./filesystem/file";
+export * from "./filesystem/filesystem";
+export * from "./filesystem/path/path";
 
-export * from "terminal/command";
-export * from "terminal/commandDriver";
-export * from "terminal/consoleHost";
-export * from "terminal/utils/log";
+export * from "./terminal/command";
+export * from "./terminal/commandDriver";
+export * from "./terminal/consoleHost";
+export * from "./terminal/utils/log";
 
-export * from "terminal/commands/file/cd";
-export * from "terminal/commands/file/ls";
-export * from "terminal/commands/file/makeDirectoryContent";
-export * from "terminal/commands/file/rm";
+export * from "./terminal/commands/file/cd";
+export * from "./terminal/commands/file/ls";
+export * from "./terminal/commands/file/makeDirectoryContent";
+export * from "./terminal/commands/file/rm";
 
-export * from "terminal/commands/user/su";
-export * from "terminal/commands/user/useradd";
-export * from "terminal/commands/user/whoami";
+export * from "./terminal/commands/user/su";
+export * from "./terminal/commands/user/useradd";
+export * from "./terminal/commands/user/whoami";
 
-export * from "terminal/commands/help";
-export * from "terminal/commands/exit";
-export * from "terminal/commands/history";
+export * from "./terminal/commands/help";
+export * from "./terminal/commands/exit";
+export * from "./terminal/commands/history";
+
+// xterm
+export * from "./xterm/addon";
--- src/xterm/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @file Exports the nebula-sh/xterm package.
- */
-
-export * from "./addon";
