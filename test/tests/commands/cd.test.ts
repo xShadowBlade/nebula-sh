124f586a5a1a4e33536780d458282454aece8d4d
zBlade <xshadowblade618@gmail.com>
2024-12-22 15:00:18 -0500

    feat(tests): add cd test, runCommand util, change to type module

--- test/tests/commands/cd.test.ts
+++ test/tests/commands/cd.test.ts
@@ -1,3 +1,16 @@
 /**
  * @file Test for cd command
  */
+import { describe, it } from "mocha";
+import { assert } from "chai";
+
+import { runCommand } from "../../utils/run";
+
+describe("cd command", () => {
+    it("should change directory", async () => {
+        const commandsToRun = ["mkdir test", "cd test"];
+        await runCommand(commandsToRun).then((computer) => {
+            assert.strictEqual(computer.consoleHost.currentWorkingDirectory.path, "/test");
+        });
+    });
+});
--- test/utils/run.ts
+++ test/utils/run.ts
@@ -1,4 +1,31 @@
 /**
  * @file Utils for running commands
  */
-import { defaultComputer } from "../../src/computer/computer";
+import { defaultComputer } from "nebula-sh";
+import type { Computer, ConsoleHost } from "nebula-sh";
+
+/**
+ * Run a series of commands.
+ * @param commands - The commands to run.
+ * @param options - The options. See {@link ConsoleHost.runCommand}.
+ * @returns The computer.
+ * @example
+ * it("should run a command", async () => {
+ *     const commandsToRun = ["mdkir test", "cd test", "pwd"];
+ *     await runCommand(commandsToRun).then((computer) => {
+ *         expect(computer.consoleHost.currentWorkingDirectory.path).toBe("/test");
+ *     });
+ * });
+ */
+export async function runCommand(
+    commands: string[],
+    options?: Parameters<ConsoleHost["runCommand"]>[1],
+): Promise<Computer> {
+    // For each command, run it
+    for (const command of commands) {
+        await defaultComputer.consoleHost.runCommand(command, options);
+    }
+
+    // Return the computer
+    return defaultComputer;
+}
