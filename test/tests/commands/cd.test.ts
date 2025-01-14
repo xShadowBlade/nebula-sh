bf2e03edaf953b10296a2216454cf7ee93b583e9
zBlade <dnguy3n2010@gmail.com>
2025-01-14 14:08:07 +0000

    feat(tests): add more cd command tests and add run options

--- test/tests/commands/cd.test.ts
+++ test/tests/commands/cd.test.ts
@@ -6,11 +6,37 @@ import { assert } from "chai";
 
 import { runCommand } from "../../utils/run";
 
+// Test the cd command
 describe("cd command", () => {
+    // Reset the computer after each test
+    afterEach(() => {
+        runCommand([], { resetComputer: true });
+    });
+
     it("should change directory", async () => {
         const commandsToRun = ["mkdir test", "cd test"];
+
         await runCommand(commandsToRun).then((computer) => {
+            // Check if the current working directory is /test
             assert.strictEqual(computer.consoleHost.currentWorkingDirectory.path, "/test");
+        });
+    });
+
+    it("should not change directory if it does not exist", async () => {
+        const commandsToRun = ["cd folderthatdoesnotexist"];
+
+        await runCommand(commandsToRun).then((computer) => {
+            // Check if the current working directory is still /
+            assert.strictEqual(computer.consoleHost.currentWorkingDirectory.path, "/");
+        });
+    });
+
+    it("should change directory to the root directory", async () => {
+        const commandsToRun = ["mkdir test", "cd test", "cd /"];
+
+        await runCommand(commandsToRun).then((computer) => {
+            // Check if the current working directory is /
+            assert.strictEqual(computer.consoleHost.currentWorkingDirectory.path, "/");
         });
     });
 });
