f7aade905832fb7746aafe7ed52cb37e6c05b342
zBlade <dnguy3n2010@gmail.com>
2025-01-15 13:43:07 +0000

    feat(test): add pwd test, refactor cd test

--- test/tests/commands/cd.test.ts
+++ test/tests/commands/cd.test.ts
@@ -2,7 +2,7 @@
  * @file Test for cd command
  */
 import { LogLevel } from "nebula-sh";
-import { describe, it } from "mocha";
+import { describe, it, beforeEach } from "mocha";
 import { assert } from "chai";
 
 import { runCommand } from "../../utils/run";
@@ -37,41 +37,44 @@ interface CdCommandTestCase {
     expectedError?: string | boolean;
 }
 
+/**
+ * Test cases for the cd command.
+ */
+const testCases: CdCommandTestCase[] = [
+    {
+        description: "should change directory",
+        commands: ["mkdir test", "cd test"],
+        expectedPath: "/test",
+    },
+    {
+        description: "should not change directory if it does not exist",
+        commands: ["cd folderthatdoesnotexist"],
+        expectedPath: "/",
+        expectedError: true,
+    },
+    {
+        description: "should change directory to the root directory",
+        commands: ["mkdir test", "cd test", "cd /"],
+        expectedPath: "/",
+    },
+    {
+        description: "should change directory to parent directory",
+        commands: ["mkdir test", "cd test", "mkdir subfolder", "cd subfolder", "cd .."],
+        expectedPath: "/test",
+    },
+    {
+        description: "should handle complex paths",
+        commands: ["mkdir test", "cd test", "mkdir subfolder", "cd subfolder", "cd ../.."],
+        expectedPath: "/",
+    },
+];
+
 // Test the cd command
 describe("cd command", () => {
-    // Reset the computer after each test
-    afterEach(() => {
+    // Reset the computer before each test
+    beforeEach(() => {
         runCommand([], { resetComputer: true });
     });
-
-    const testCases: CdCommandTestCase[] = [
-        {
-            description: "should change directory",
-            commands: ["mkdir test", "cd test"],
-            expectedPath: "/test",
-        },
-        {
-            description: "should not change directory if it does not exist",
-            commands: ["cd folderthatdoesnotexist"],
-            expectedPath: "/",
-            expectedError: true,
-        },
-        {
-            description: "should change directory to the root directory",
-            commands: ["mkdir test", "cd test", "cd /"],
-            expectedPath: "/",
-        },
-        {
-            description: "should change directory to parent directory",
-            commands: ["mkdir test", "cd test", "mkdir subfolder", "cd subfolder", "cd .."],
-            expectedPath: "/test",
-        },
-        {
-            description: "should handle complex paths",
-            commands: ["mkdir test", "cd test", "mkdir subfolder", "cd subfolder", "cd ../.."],
-            expectedPath: "/",
-        },
-    ];
 
     testCases.forEach((testCase) => {
         // Run the test case with the given description
---
+++ test/tests/commands/pwd.test.ts
@@ -0,0 +1,77 @@
+/**
+ * @file Test for pwd command
+ */
+import { LogLevel } from "nebula-sh";
+import { describe, it, beforeEach } from "mocha";
+import { assert } from "chai";
+
+import { runCommand } from "../../utils/run";
+import { expectMessage } from "../../utils/messages";
+
+/**
+ * A test case for the pwd command.
+ */
+interface PwdCommandTestCase {
+    /**
+     * The name/description of the test case.
+     * @example "should print working directory"
+     */
+    description: string;
+
+    /**
+     * The commands to run.
+     * @example ["pwd"]
+     */
+    commands: string[];
+
+    /**
+     * The expected output of the command.
+     * @example "/test"
+     */
+    expectedMessage: string;
+}
+
+/**
+ * Test cases for the pwd command.
+ */
+const testCases: PwdCommandTestCase[] = [
+    {
+        description: "should print working directory",
+        commands: ["pwd"],
+        expectedMessage: "/",
+    },
+    {
+        description: "should print working directory after changing",
+        commands: ["mkdir test", "cd test", "pwd"],
+        expectedMessage: "/test",
+    },
+    {
+        description: "should print working directory after changing to root",
+        commands: ["mkdir test", "cd test", "cd /", "pwd"],
+        expectedMessage: "/",
+    },
+    {
+        description: "should print working directory for a complex path",
+        commands: ["mkdir test", "cd test", "mkdir folder", "cd folder", "pwd"],
+        expectedMessage: "/test/folder",
+    },
+];
+
+describe("pwd command", () => {
+    // Reset the computer before each test
+    beforeEach(() => {
+        runCommand([], { resetComputer: true });
+    });
+
+    testCases.forEach((testCase) => {
+        it(testCase.description, async () => {
+            const { commands, expectedMessage } = testCase;
+
+            // Run the commands
+            await runCommand(commands);
+
+            // Expect the message
+            expectMessage(LogLevel.Log, expectedMessage);
+        });
+    });
+});
